4d76a71197bb9b0173a7f95fa7fd6296631e8046
DienPutriAlexa <dienputrialexa17@gmail.com>
2026-09-04 15:52:34 +0700

    latihan modul lanjutan

--- 03_Modul/01Latihan_Modul/data_toko.py
+++ 03_Modul/01Latihan_Modul/data_toko.py
@@ -48,10 +48,32 @@ def cari_barang_termahal(belanjaan):
     return barang_mahal
 
 def cari_barang_termurah(belanjaan):
-    barang_murah = belanjaan[0]
+    barang_murah = belanjaan[0] #Sebelum mulai membandingkan satu per satu, komputer mengambil barang yang paling pertama (urutan ke-0 di dalam list) untuk dijadikan "pembanding awal" atau diasumsikan sementara sebagai barang yang paling murah.
 
-    for barang in belanjaan:
-        if barang["harga"] < barang_murah["harga"]:
-            barang_murah = barang
+    for barang in belanjaan: #Ini adalah perintah perulangan (loop) untuk memeriksa seluruh isi barang yang ada di dalam belanjaan satu persatu dari awal sampai akhir.
+        if barang["harga"] < barang_murah["harga"]: #Apakah harga barang yang sedang diperiksa saat ini lebih kecil (<) daripada harga barang_murah yang disimpan sebelumnya?"
+            barang_murah = barang #Jika ternyata ada barang yang harganya lebih murah dari simpanan sebelumnya, komputer akan memperbarui variabel barang_murah dengan data barang yang baru ini. Tapi kalau tidak lebih murah, baris ini dilewati.
 
     return barang_murah
+
+def cari_sesuai_budget(belanjaan, budget):
+    #buat list kosong untuk menampung barang-barang yang harganya cocok
+    barang_pilihan = []
+
+    for barang in belanjaan:
+        #Jika harga barang kurang dari atau sama dengan budget
+        if barang["harga"] <= budget:
+            #masukkan barang tersebut ke dalam list barang_pilihan
+            barang_pilihan.append(barang)
+
+    #kembalikan list yang sudah berisi barang-barang hasil saringan
+    return barang_pilihan
+
+def cari_barang_berdasarkan_jumlah_beli(belanjaan):
+    barang_beli = []
+
+    for barang in belanjaan:
+        if barang["jumlah"] > 2:
+            barang_beli.append(barang)
+
+    return barang_beli
---
+++ 03_Modul/01Latihan_Modul/main4.py
@@ -0,0 +1,35 @@
+import data_toko
+
+print("===STRUK BELANJA TOKO DIEN===\n")
+print(f"Nama Pembeli :{data_toko.keranjang['Nama_Pembeli']}")
+
+total_barang = 0
+for nomor,barang in enumerate(data_toko.keranjang["Items"], start=1):
+    subtotal_perproduk = barang["harga"] * barang["jumlah"]
+    total_barang = total_barang + subtotal_perproduk #atau ditulis seperti ini --> total_barang += subtotal_perproduk
+
+    print(f"{nomor}.Produk : {barang['nama_produk']}  | Harga : {barang['harga']}  | Jumlah : {barang['jumlah']}  | Total Harga : {subtotal_perproduk}")
+
+
+
+Total_keseluruhan = data_toko.hitung_total(data_toko.keranjang["Items"])
+print(f"Total Belanja Keseluruhan : {Total_keseluruhan}")
+
+
+# ini memanggil fungsi barang termahal dan mencetaknya
+produk_mahal = data_toko.cari_barang_termahal(data_toko.keranjang["Items"])
+print(f"Barang termahal adalah : {produk_mahal}")
+
+#ini memanggil fungsi barang termurah dan mencetaknya
+produk_murah = data_toko.cari_barang_termurah(data_toko.keranjang["Items"])
+print(f"Barang termurah adalah : {produk_murah}")
+
+#ini yang menjadi pembeda dengan main3.py
+uang_saya = 1500000
+hasil_pencarian = data_toko.cari_sesuai_budget(data_toko.keranjang["Items"], uang_saya)
+
+print(f"\n --- BARANG DENGAN HARGA DI BAWAH Rp {uang_saya} ---")
+for barang in hasil_pencarian:
+    print(f"- {barang['nama_produk']} : Rp {barang['harga']}")
+
+print("\n===TERIMA KASIH TELAH BERBELANJA DI TOKO DIEN===")
